f159b3b560c68e695e5b2b0a4c2dbd992e8d670d
StanSNS <stanimirsergev159@gmail.com>
2024-01-22 13:16:50 +0200

    feature/Interest created the whole component.

---
+++ src/Component/Interest/Interest.jsx
@@ -0,0 +1,21 @@
+import './Interest.css'
+import interestsData from "../../Resources/Data/interests";
+
+function Interest() {
+    return (
+        <div className="workSection">
+            <h1 className="text-center textColor">Interests</h1>
+            <div className="interestSection">
+                {interestsData.map((interest, index) => (
+                    <div className="interestPart">
+                        <span className="interestIcon">{<interest.icon/>}</span>
+                        <p className="interestText" key={index}>{interest.name}</p>
+
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Interest;
