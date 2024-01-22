9c05530c60daf5ee70495756774edbdd0952d954
StanSNS <stanimirsergev159@gmail.com>
2024-01-22 22:20:25 +0200

    feature/Minor-bug-fixes modified responsivness in project section.

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -16,8 +16,7 @@ function Project() {
 
     return (
         <div className="projectDimension">
-            <h1 className="text-center mt-1  mb-5 ">Projects</h1>
-
+            <h1 className="text-center mt-1  mb-3 ">Projects</h1>
             <div className="projectSection ">
                 {projectData.map((project, index) => (
                     <div className="projectCard" key={index}>
