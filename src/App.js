6d5d06dfa9f1a9b1e58669c4cc548465f4410614
StanSNS <stanimirsergev159@gmail.com>
2024-01-23 18:41:56 +0200

    deleted Error404 component and fixed the routing.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./index.css"
 
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Footer from "./Component/Footer/Footer";
 import Header from "./Component/Header/Header";
 import Skill from "./Component/Skill/Skill";
@@ -9,29 +9,14 @@ import Education from "./Component/Education/Education";
 import Work from "./Component/Work/Work";
 import Project from "./Component/Project/Project";
 import Interest from "./Component/Interest/Interest";
-import Error404 from "./Component/Error404/Error404";
 
 const App = () => {
     return (
-        <div className="foundationBackgroundColor">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={
-                        <>
-                            <Header/>
-                            <Skill/>
-                            <Education/>
-                            <Work/>
-                            <Project/>
-                            <Interest/>
-                            <Footer/>
-                        </>
-                    }/>
-                    <Route path="*" element={<Navigate to="/404"/>}/>
-                    <Route path="/404" element={<Error404/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/StanimirSergevCV" element={<><Header/><Skill/><Education/><Work/><Project/><Interest/><Footer/></>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 export default App;
